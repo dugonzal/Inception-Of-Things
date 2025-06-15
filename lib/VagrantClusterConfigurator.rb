ac269f07f8f4c24241bb5d652815695eaea39042
dugonzal <dugonzal@protonmail.com>
2025-06-15 13:51:36 +0200

    feat: clarificando

--- lib/VagrantClusterConfigurator.rb
+++ lib/VagrantClusterConfigurator.rb
@@ -5,13 +5,13 @@
 
 require_relative 'Utils'
 
-class VagrantClusterConfigurator < Utils
+class VagrantClusterConfigurator
   DEFAULT_VERBOSE   = '--verbose'
   DEFAULT_PATH      = 'confs/config.json'
   DEFAULT_SCRIPT    = 'scripts/bootstrap.sh'
-  DEFAULT_CFG = "scripts/ansible.cfg"
+  DEFAULT_CFG       = "scripts/ansible.cfg"
   DEFAULT_PLAYBOOK  = 'scripts/ansible/playbook/site.yml'
-  DEFAULT_INVENTORY = 'scripts/ansible/inventory/inventory.ini'
+  DEFAULT_INVENTORY = 'scripts/ansible/inventory/dev/inventory.ini'
   attr_reader :nodes, :path, :script, :verbose, :playbook, :inventory, :cfg
 
   def initialize(path: DEFAULT_PATH, inventory: DEFAULT_INVENTORY, playbook: DEFAULT_PLAYBOOK, script: DEFAULT_SCRIPT, verbose: DEFAULT_VERBOSE, cfg: DEFAULT_CFG)
@@ -28,8 +28,6 @@ class VagrantClusterConfigurator < Utils
     generate
   end
 
-  private
-
   private def generate
     raise 'No nodes defined' if @nodes.empty?
 
@@ -43,7 +41,7 @@ class VagrantClusterConfigurator < Utils
     end
   end
 
-  private  def provision_node(config, node)
+  private def provision_node(config, node)
     config.vm.define node[:name] do |machine|
       machine.vm.hostname = node[:hostname]
       machine.vm.network 'private_network', ip: node[:network_address]
@@ -54,22 +52,22 @@ class VagrantClusterConfigurator < Utils
     end
   end
 
-  private  def provision_ansible(machine, privileged: true)
+  private def provision_ansible(machine, privileged: true)
     machine.vm.provision 'shell', path: @script, privileged: privileged if @script
     machine.vm.provision 'ansible' do |ansible|
+      ansible.config_file    = @cfg
+      ansible.verbose        = @verbose
+      ansible.playbook       = @playbook
       ansible.inventory_path = @inventory
-      ansible.playbook = @playbook
-      ansible.verbose = @verbose
-      ansible.config_file  = @cfg
-      ansible.become = true if privileged
+      ansible.become         = true if privileged
     end
   end
 
-  private  def provision_libvirt(machine, node)
+  private def provision_libvirt(machine, node)
     machine.vm.provider :libvirt do |lv|
+      lv.cpus = node[:cpus]
       lv.default_prefix = ''
       lv.memory = node[:memory]
-      lv.cpus = node[:cpus]
     end
   end
 end
